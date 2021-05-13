d99736b2db71484e14a7a71b99cd2d999c10baaf
LougerB <glouien@gmail.com>
2021-05-14 06:46:13 +0800

    edit data location

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const recipeWrapper = document.getElementById("recipe"),
   foodWrapper = document.querySelector(".food");
 
 async function getRecipe() {
-  const fetchRecipe = await fetch("data.json");
+  const fetchRecipe = await fetch("./data.json");
   const recipe = await fetchRecipe.json();
   console.log(recipe.recipes);
 }
