134a1073475321ca09b6c71082c5989b6c66cd7b
Lougerb <64685319+Lougerb@users.noreply.github.com>
2021-05-14 06:37:45 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const recipeWrapper = document.getElementById("recipe"),
+  foodWrapper = document.querySelector(".food");
+
+async function getRecipe() {
+  const fetchRecipe = await fetch("data.json");
+  const recipe = await fetchRecipe.json();
+  console.log(recipe.recipes);
+}
+getRecipe();
+// DOMQuad.///
