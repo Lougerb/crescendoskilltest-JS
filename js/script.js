7bc1138165ff0c5da485eb076da30e1eeb22d04d
LougerB <glouien@gmail.com>
2021-05-14 13:03:54 +0800

    api recipe working

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ async function getRecipe() {
   // console.log(recipe.recipes);
 
   const recipeList = recipe.recipes;
-  // console.log(recipeList);
 
   recipeList.forEach((food) => {
     // Datastruct
@@ -20,12 +19,10 @@ async function getRecipe() {
       description,
       images: { full: imgFull, medium: imgMed, small: imgSM },
       servings,
-      preptime,
-      cooktime,
-      ingredients: [
-        { uuid: ingredientsID, amount, measurement, name: ingredientName },
-      ],
-      directions: [{ instructions, optional }],
+      prepTime,
+      cookTime,
+      ingredients,
+      directions,
       postDate,
       editDate,
     } = food;
@@ -34,28 +31,71 @@ async function getRecipe() {
     // Copy the node
     const newRecipe = document.importNode(recipeWrapper.content, true);
     // Defining elements to be inserted by datas
-    const foodUUID = document.querySelector(".food-id");
-    const foodTitle = document.querySelector(".food-title");
-    const foodDesc = document.querySelector(".description");
-    const foodServ = document.querySelector(".servings");
-    // const foodCookTime = document.querySelector(".cooktime");
-    // const foodCookTime = document.querySelector(".cooktime");
-    // const foodCookTime = document.querySelector(".cooktime");
-    // const foodCookTime = document.querySelector(".cooktime");
-    // const foodCookTime = document.querySelector(".cooktime");
-    // const foodUUID = document.querySelector(".food-title");
-    // console.log(foodID);
+    const foodUUID = newRecipe.querySelector(".food-id");
 
+    const foodTitle = newRecipe.querySelector(".food-title");
+    const foodDesc = newRecipe.querySelector(".description");
+    const foodServ = newRecipe.querySelector(".servings");
+    const foodPrepTime = newRecipe.querySelector(".preptime");
+    const foodCookTime = newRecipe.querySelector(".cooktime");
+    const foodimg = newRecipe.querySelector(".food-img");
+
+    const foodPostTime = newRecipe.querySelector(".post");
+    const foodEditTime = newRecipe.querySelector(".edit");
+    // console.log(foodID);
+    foodimg.setAttribute("src", imgMed);
+    foodUUID.textContent = foodID;
+    foodTitle.textContent = title;
+    foodDesc.textContent = description;
+    foodServ.textContent = servings;
+    foodPrepTime.textContent = prepTime;
+    foodCookTime.textContent = cookTime;
+
+    Object.entries(ingredients).forEach((ingredientArr, ingredientKey) => {
+      const [
+        ingredientNum,
+        { uuid: ingredientsID, amount, measurement, name: ingredientName },
+      ] = ingredientArr;
+      const ingredientListTemplate = newRecipe.querySelector(
+        ".ingredient-template"
+      );
+      const ingredientList = newRecipe.querySelector(".ingredient");
+      const newIngredient = document.importNode(
+        ingredientListTemplate.content,
+        true
+      );
+      const ingredientTitle = newIngredient.querySelector(".ingredient-title");
+      const ingredientID = newIngredient.querySelector(".ingredient-id");
+      const ingredientAmount = newIngredient.querySelector(".amount");
+      const ingredientMeasure = newIngredient.querySelector(".measurement");
+      ingredientTitle.textContent = ingredientName;
+      ingredientID.textContent = ingredientsID;
+      ingredientAmount.textContent = amount;
+      ingredientMeasure.textContent = measurement;
+      ingredientList.appendChild(newIngredient);
+
+      // const sampleID = "3d810ba9-7e4e-48aa-b2e9-7918e38b358d";
+
+      // if (sampleID == ingredientsID) {
+      //   console.log(ingredientName);
+      // }
+    });
+    Object.entries(directions).forEach((directArr, directKey) => {
+      const [num, { instructions, optional }] = directArr;
+      const directTemplate = newRecipe.querySelector(".direction-template");
+      const directList = newRecipe.querySelector(".direction-list");
+      const newDirect = document.importNode(directTemplate.content, true);
+      const directInstruct = newDirect.querySelector(".instruction");
+      const directOption = newDirect.querySelector(".optional");
+
+      directInstruct.textContent = instructions;
+      directOption.textContent = optional ? "Yes" : "No";
+      directList.appendChild(newDirect);
+    });
+
+    foodPostTime.textContent = postDate;
+    foodEditTime.textContent = editDate;
     recipieSection.appendChild(newRecipe);
-    // foodUUID.textContent = `${foodID}`;
-    // foodTitle.textContent = `${title}`;
-    // foodDesc.textContent = description;
-    // foodServ.textContent = servings;
-    foodUUID.textContent = `123123123123`;
-    foodTitle.textContent = `xd title`;
-    foodDesc.textContent = `lorem10`;
-    foodServ.textContent = `4`;
-    // recipieSection.appendChild(newRecipe);
   });
 }
 getRecipe();
